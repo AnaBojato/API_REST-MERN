981db4f35704264958ea5dd114b36196fa27fdb2
AnaBojato <160247277+AnaBojato@users.noreply.github.com>
2025-06-03 19:32:50 -0500

    🔧 Configure CORS and MongoDB connection

--- mi-proyecto-node/app.js
+++ mi-proyecto-node/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,6 +10,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Para analizar objetos JSON en el body
 app.use(bodyParser.json());
 
+app.use(
+  cors({
+    origin: "*", // Permite acceso desde cualquier dominio
+  })
+);
+
+
 // Ruta simple para verificar que el servidor funciona
 app.get("/status", (req, res) => {
   res.status(200).send({
@@ -16,5 +24,21 @@ app.get("/status", (req, res) => {
     message: "Servidor Corriendo",
   });
 });
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const MONGODB_URI = `mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DB}`;
+
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Base de Datos Conectada");
+  })
+  .catch((err) => {
+    console.log(err);
+    throw err;
+  });
 
 module.exports = app;
